9008f7a38e3f009b112da166f39177be2fc89ca8
e2dani <e2codeprotonmail.com>
2023-10-18 22:04:10 -0300

    inicio de frontend

--- frontend/src/views/home.jsx
+++ frontend/src/views/home.jsx
@@ -1,70 +1,46 @@
 import { Link } from "react-router-dom";
 
 export function HomeXD() {
-  // const {Mks} = useContext(context)
-  // console.log(Mks)
   return (
     <div className="bxGenHome">
+
+      <div className="bxlocal">
+        <div>
+          <h2>anuncios</h2>
+        </div>
+        <div>
+          <h2>dolar hoy</h2>
+        </div>
+        <div>
+          <h2>tiempo</h2>
+        </div>
+      </div>
+
+      <div className="trend">
+        <div className=""><img src="src/assets/stallman.jpg" alt="" /></div>
+        <div className=""><img src="src/assets/murphy.png" alt="" /></div>
+        <div className=""><img src="src/assets/socrates.jpg" alt="" /></div>       
+      </div>
+
+      <div className="bxNTall">
+        <div className="bxNTdeldia">
+
+        </div>
+
+        <div className="bxNTvar">
+          <div className="bxNT">
+            <img src="src/assets/stallman.jpg" alt="" />
+            <div className="tt">
+              <h1>mañana llueve</h1>
+            </div>
+            <div className="des">
+              <p>naa es mentira, el pronostico dice otra cosa pero estaremos atentos a los cambios</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
       <Link to ='/dios' >creadores</Link>
     </div>
   )
 }
-
-
-// import {UseHook} from '../context/atomCtx'
-// //!! LA IMPORTANCIA DE { UseHook } y en modo CamelCase
-
-// import { Link } from "react-router-dom";
-// //? CAMBIAR DE PAGINA SIN REFRESCAR Y/O CONSERVAR DATOS
-
-// import {AtomBx} from "../components/atomBx";
-
-// export function HomeXD() {
-
-//   const { XYZ } = UseHook() //IMPORTANTE PARA ENTREGAR LA LONGITUD/CANTIDAD DE ATOMOS
-
-  
-
-//   const renderMain = () => {
-//     if (XYZ.length === 0) return (
-//       <div>
-//         <h1>no hay atomos</h1>
-//       </div>
-//     );
-
-//     return (
-//       <div>
-//         {/* HomeXD diario */}
-//         {XYZ.map(hadron => (
-//           <AtomBx hadron={hadron} key={hadron._id}></AtomBx>
-//         ))}
-
-//         {/* <Link to='/add'>ir a add</Link> */}
-//         {/* <button className="bg-sky-500 hover:bg-sky-700" onClick={() => setMks([1,2,3])}>   */}
-//         {/*SE EJECUTA CUANDO DOY CLICK       */}
-//           {/* add */}
-//         {/* </button>
-//         <p className="text-blue-600">hola mundo</p> */}
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className='bxGen'>
-//       <div className='bxAtomStatus'>
-//         <h1>cantidad de atomos ({XYZ.length})</h1>
-//         <Link to ='/addy' >Crear nuevo atomo</Link>
-//       </div>
-
-      
-//       {renderMain()}
-//       {/* RETORNA EL ESTADO DE ATOMOS. RENDERIZARA Y CONSERVARA UNA PORCION DEL ADD */}
-      
-//     </div>
-    
-//   )
-// }
-
-// //!! EL OBJETIVO DELEGAR LA EXPORTACION DE FUNCIONES Y PARAMETROS  A , view/index.js 
-// //!! Y NO FICHERO POR SEPARADO
-// // export default home 
