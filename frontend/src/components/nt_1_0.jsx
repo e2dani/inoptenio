074ce1eb4eff2ea761e2fb679fef222d2be8a06c
e2dani <e2codeprotonmail.com>
2023-11-17 12:05:14 -0300

    nt tuneado

--- frontend/src/components/nt_1_0.jsx
+++ frontend/src/components/nt_1_0.jsx
@@ -3,7 +3,17 @@ export function NT_1_0({hadron}) {
     return (
     
         <div className="nt">
-            <div className="NTbx0">        
+            <div className="NTbx0">     
+                <div className="NTdata">
+                    <div className="xd">
+                        <div className="imgxd">
+                            <img src="public/socrates.jpg" alt="" />
+                        </div>                        
+                        <h1>Dani</h1>
+                    </div>
+                    <div className="link_nt"><a href="#">Link</a></div>
+                </div>   
+                
                 {/* <div className="NTtt"><h1>{hadron.tt}</h1></div>                     */}
                 <div className="NTimg"><img className="imgNT" src={hadron.img.EO_url} alt="" /></div>
                 <div className="NTtt"><h1>{hadron.tt}</h1></div>                    
